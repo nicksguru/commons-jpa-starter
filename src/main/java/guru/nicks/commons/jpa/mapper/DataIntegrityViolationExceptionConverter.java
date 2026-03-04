0193bc45a7e1c37d6bbd98fe73f5a38d841054be
Nick S. <job-offers@nicks.guru>
2026-03-04 15:23:25 +0300

    feat: track duplicate key value as AlreadyExistsException

--- src/main/java/guru/nicks/commons/jpa/mapper/DataIntegrityViolationExceptionConverter.java
+++ src/main/java/guru/nicks/commons/jpa/mapper/DataIntegrityViolationExceptionConverter.java
@@ -1,5 +1,6 @@
 package guru.nicks.commons.jpa.mapper;
 
+import guru.nicks.commons.exception.AlreadyExistsException;
 import guru.nicks.commons.exception.ExceptionConverter;
 import guru.nicks.commons.exception.http.ConflictException;
 
@@ -7,4 +8,18 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 public class DataIntegrityViolationExceptionConverter
         implements ExceptionConverter<DataIntegrityViolationException, ConflictException> {
+
+    /**
+     * If the exception message contains 'duplicate key value', it's mapped to a more specific
+     * {@link AlreadyExistsException}.
+     */
+    @Override
+    public ConflictException convert(DataIntegrityViolationException cause) {
+        return (cause != null)
+                && (cause.getMessage() != null)
+                && cause.getMessage().contains("duplicate key value")
+                ? new AlreadyExistsException(cause)
+                : ExceptionConverter.super.convert(cause);
+    }
+
 }
