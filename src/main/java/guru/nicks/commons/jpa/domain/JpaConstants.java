352a762f6b726067b9df97601631c318dfd2879e
Nick S. <job-offers@nicks.guru>
2026-06-04 19:33:00 +0300

    refactor!: remove ONE_TO_MANY_BATCH_SIZE

--- src/main/java/guru/nicks/commons/jpa/domain/JpaConstants.java
+++ src/main/java/guru/nicks/commons/jpa/domain/JpaConstants.java
@@ -6,22 +6,21 @@ import org.hibernate.annotations.BatchSize;
 
 /**
  * Handy constants for JPA entity declarations.
+ * <p>
+ * For making all lazy associations use batch fetching by default
+ * ({@code SELECT * FROM child_table WHERE parent_id IN (1, 2, 3)} without using {@link BatchSize} explicitly, set
+ * {@code spring.jpa.properties.hibernate .default_batch_fetch_size} in application properties to, for example, 50.
  */
 @UtilityClass
 public class JpaConstants {
 
     /**
-     * For use in {@link OrderColumn#name()}.
+     * For use in {@link OrderColumn#name()} to make {@code List<SomeEntity>} maintain order.
      */
     public static final String PERSISTENT_LIST_ORDER_COLUMN = "offsetInList";
 
     /**
-     * Recommented value for {@link BatchSize#size()} to mitigate the N+1 problem when dealing with lazy collections.
-     */
-    public static final int ONE_TO_MANY_BATCH_SIZE = 10;
-
-    /**
-     * Recommended value for <b>internal</b> paginated queries.
+     * Recommended value for <b>internal</b> (not UI-facing) paginated queries.
      */
     public static final int INTERNAL_PAGE_SIZE = 500;
 
