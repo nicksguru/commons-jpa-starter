3077e2abfce67d41847c91b3d5d65d3ebeb21345
Nick S. <job-offers@nicks.guru>
2026-07-28 11:50:47 +0300

    feat: add withoutAutoFlushMode method

--- src/main/java/guru/nicks/commons/jpa/JpaInference.java
+++ src/main/java/guru/nicks/commons/jpa/JpaInference.java
@@ -1,23 +1,32 @@
 package guru.nicks.commons.jpa;
 
+import guru.nicks.commons.exception.user.EmailAlreadyExistsException;
 import guru.nicks.commons.jpa.domain.EnhancedSqlDialect;
+import guru.nicks.commons.utils.TransformUtils;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.FlushModeType;
+import jakarta.persistence.Table;
 import jakarta.persistence.metamodel.EntityType;
 import jakarta.persistence.metamodel.Metamodel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
 import org.hibernate.boot.model.naming.Identifier;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.lang.reflect.Field;
+import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
 /**
- * Utility class for inferring JPA table and column names. Requires {@link EntityManager} for JPA Metamodel.
+ * Utility class for inferring JPA table and column names. There's also a {@link #withoutAutoFlushMode(Supplier)}
+ * utility method.
  */
 @RequiredArgsConstructor
 @Slf4j
@@ -61,15 +70,16 @@ public class JpaInference {
      */
     public String getTableName(Class<?> entityClass) {
         String tableName;
+        Table tableAnnotation = entityClass.getAnnotation(Table.class);
 
-        jakarta.persistence.Table tableAnnotation = entityClass.getAnnotation(jakarta.persistence.Table.class);
+        // explicit table name set
         if ((tableAnnotation != null) && !tableAnnotation.name().isBlank()) {
             tableName = tableAnnotation.name();
             // honor SQL quoting
             Identifier identifier = Identifier.toIdentifier(tableName);
             tableName = identifier.render();
         }
-        // fallback on inferring table name from class name using JPA metamodel
+        // infer table name from class name using JPA metamodel
         else {
             Metamodel metamodel = entityManager.getMetamodel();
             EntityType<?> entityType = metamodel.entity(entityClass);
@@ -121,6 +131,33 @@ public class JpaInference {
         String snakeCase = CAMEL_CASE_TO_SNAKE_CASE.matcher(camelCase).replaceAll("$1_$2").toLowerCase();
         Identifier identifier = Identifier.toIdentifier(snakeCase);
         return identifier.render();
+    }
+
+    /**
+     * Executes a given code with the Hibernate session's flush mode temporarily set to {@link FlushModeType#COMMIT}
+     * (vs. the default {@link FlushMode#AUTO}). This is necessary for preventing unintended flushes before a specific
+     * operation.
+     * <p>
+     * For example, let a new entity have an email address set, and there's a unique DB constraint for it. If the entity
+     * is saved, a general-purpose {@link DataIntegrityViolationException} will be raised. But we should display a
+     * meaningful 'Email already exists' message, which requires a {@link EmailAlreadyExistsException} to be raised. The
+     * solution is to find an entity having the same email address <b>without saving the new entity</b>.
+     *
+     * @param supplier code to be executed
+     * @param <T>      supplier result type
+     * @return what the supplier returns
+     * @see TransformUtils#toSupplier(Runnable)
+     */
+    public <T> T withoutAutoFlushMode(Supplier<T> supplier) {
+        Session session = entityManager.unwrap(Session.class);
+        FlushModeType oldFlushMode = session.getFlushMode();
+
+        try {
+            session.setFlushMode(FlushModeType.COMMIT);
+            return supplier.get();
+        } finally {
+            session.setFlushMode(oldFlushMode);
+        }
     }
 
 }
